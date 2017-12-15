f9daa5bfdf34f5fdc96d8fe526e0217bc31698e5
Alireza Shahabi <shahabvshahabi1996@gmail.com>
2017-12-15 14:35:35 +0330

    added provider to App.js

--- App.js
+++ App.js
@@ -1,13 +1,15 @@
 import React , {Component} from 'react';
-import {Text , View} from 'react-native';
-
+import { Text , View } from 'react-native';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 
 export default class App extends Component{
   render(){
     return(
-      <View>
-        <Text>Hello World</Text>
-      </View>
+      <Provider store={createStore}>
+        <View>
+        </View>
+      </Provider>
     );
   }
 }
